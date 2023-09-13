d0c2b8d4fa3f9097ef4b2746be0fad49f448dddd
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-13 20:54:39 +0100

    getting started

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+// db
+const connectDB = require("./db/connect");
+
+// Start
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port, () => {
+      console.log(`App listening on port : ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
