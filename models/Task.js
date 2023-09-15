1f6e25fb474f29c3e00db6ad83cf0d73edc59f72
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-15 19:47:35 +0100

    working on controllers

--- models/Task.js
+++ models/Task.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const TaskSchema = mongoose.Schema({
   name: {
     type: String,
-    unique: true,
-    index: true,
     required: [true, "Please provide task name"],
     maxlength: [20, "name can not be more than 20 charachter"],
     trim: true,
